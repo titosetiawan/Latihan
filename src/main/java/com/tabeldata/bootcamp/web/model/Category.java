acd2247ee5ac9f708673b23a0b6b7f58d89fcd8d
Tito Frianto Setiawan <51995195+titosetiawan@users.noreply.github.com>
2022-03-02 13:57:29 +0700

    Add files via upload

---
+++ src/main/java/com/tabeldata/bootcamp/web/model/Category.java
@@ -0,0 +1,12 @@
+package com.tabeldata.bootcamp.web.model;
+
+import lombok.Data;
+
+@Data
+public class Category {
+
+    private String category_id;
+    private String department_id;
+    private String name;
+    private String description;
+}
